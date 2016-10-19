08a6dc1cdeab59469889604e163eb116e4c09390
akshay61 <kulkarni.ak@husky.neu.edu>
2016-10-19 01:34:57 -0700

    Second Commit
    Pushing code for User Comments

--- public/server/app.js
+++ public/server/app.js
@@ -7,4 +7,10 @@ var mongoose = require('mongoose');
 module.exports = function(app, db, userModel){
 
     require("./services/user.service.server.js")(app, userModel);
+
+    var commentsModel = require("./models/comments.model.server.js")(db, mongoose);
+    require("./services/comments.service.server.js")(app, commentsModel);
+
+    var questionModel = require("./models/questions.model.server.js")(db, mongoose);
+    require("./service/questions.service.server.js")(app, questionModel);
 }
