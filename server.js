82750f479f32336321f747c5e31d0cf90e79966a
Aravindh <eswaran.a@husky.neu.edu>
2016-11-12 16:59:40 -0800

    Changes to package.json and server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require("http");
+var mongoose = require("mongoose");
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -7,7 +9,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
 
+var connectionString = 'mongodb://localhost:27017/test';
+
+if(process.env.MLAB_DB_USERNAME) {
+    connectionString = process.env.MLAB_DB_URL_INIT +
+        process.env.MLAB_DB_USERNAME + ":" +
+        process.env.MLAB_DB_PASSWORD +
+        process.env.MLAB_DB_URL_END + '/' +
+        process.env.MLAB_DB_NAME;
+}
+
+
+mongoose.connect(connectionString);
+var db = mongoose.connection;
+
+require("./public/server/app.js")(app, mongoose, db);
+
 app.set('ipaddress', (process.env.IP));
 app.set('port', (process.env.PORT || 5000));
-
 app.listen(app.get('port'), app.get('ipaddress'));
