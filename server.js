4f9a32a8daf0aafddc139c1e38d7c4a74428f343
songy23 <songy23@ccs.neu.edu>
2016-11-23 14:44:14 -0800

    Add dist folder

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 var express = require('express');
 var app = express();
+var subpath = express();
 var http = require("http");
 var mongoose = require("mongoose");
+var url = require('url');
+var argv = require('minimist')(process.argv.slice(2));
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/v1", subpath);
+
+var swagger = require("swagger-node-express").createNew(subpath);
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static('dist'));
+subpath.use(express.static('dist'));
+
+swagger.setApiInfo({
+        title: "example API",
+        description: "API to do something, manage something...",
+        termsOfServiceUrl: "",
+        contact: "yourname@something.com",
+        license: "",
+        licenseUrl: ""
+    });
+
+subpath.get('/', function (req, res) {
+    res.sendfile(__dirname + '/dist/index.html');
+});
 
 var connectionString = 'mongodb://localhost:27017/test';
 
@@ -25,6 +46,17 @@ var db = mongoose.connection;
 
 require("./public/server/app.js")(app, mongoose, db);
 
+swagger.configureSwaggerPaths("", "/api-docs", "");
+var domain = 'localhost';
+if(argv.domain !== undefined)
+    domain = argv.domain;
+else
+    console.log('No --domain=xxx specified, taking default hostname "localhost".');
+
+var port = (process.env.PORT || 5000);
+var applicationUrl = 'http://' + domain + ':' + port;
+swagger.configure(applicationUrl, '1.0.0');
+
 app.set('ipaddress', (process.env.IP));
-app.set('port', (process.env.PORT || 5000));
+app.set('port', port);
 app.listen(app.get('port'), app.get('ipaddress'));
